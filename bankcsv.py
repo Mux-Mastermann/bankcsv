de1fcd5b11e3a2d0d4af1fccb970ae169b04059a
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-06-06 21:57:40 +0200

    Loop through all accounts

--- bankcsv.py
+++ bankcsv.py
@@ -9,46 +9,60 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from config import pw, user
 
 
-# set webdriver
-driver = webdriver.Chrome("C:/Users/Jan/dev/bankcsv/chromedriver/chromedriver")
+FROM_DATE = "05.06.2020"
+TO_DATE = "05.06.2020"
 
-# go on DKB Banking website
-driver.get('https://www.dkb.de/banking')
 
-# get field for login and password
-anmeldename = driver.find_element_by_id('loginInputSelector')
-passwort = driver.find_element_by_id("pinInputSelector")
+def main():
 
-# fill in login and password
-anmeldename.send_keys(user)
-passwort.send_keys(pw)
-# submit login form
-passwort.send_keys(Keys.RETURN)
+    # set webdriver
+    driver = webdriver.Chrome("C:/Users/Jan/dev/bankcsv/chromedriver/chromedriver")
 
-# check if logged in, by looking for overall balance
-overall_balance = WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.ID, "overallBalance"))
-)
+    # go on DKB Banking website
+    driver.get('https://www.dkb.de/banking')
 
-# navigate to Kontoumsätze
-driver.get("https://www.dkb.de/banking/finanzstatus/kontoumsaetze")
+    # get field for login and password
+    anmeldename = driver.find_element_by_id('loginInputSelector')
+    passwort = driver.find_element_by_id("pinInputSelector")
 
-# Select Account from dropdown menu
-select = Select(driver.find_element_by_id("id-1615473160_slAllAccounts"))
-select.select_by_visible_text("DE42 1203 0000 1051 2654 92 / Girokonto")
+    # fill in login and password
+    anmeldename.send_keys(user)
+    passwort.send_keys(pw)
+    # submit login form
+    passwort.send_keys(Keys.RETURN)
 
-# set date range
-from_date = driver.find_element_by_id("id-1615473160_transactionDate")
-from_date.clear()
-from_date.send_keys("01.06.2020")
-to_date = driver.find_element_by_id("id-1615473160_toTransactionDate")
-to_date.clear()
-to_date.send_keys("04.06.2020")
+    # check if logged in, by looking for overall balance
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.ID, "overallBalance"))
+    )
 
-# click on search button
-driver.find_element_by_id("searchbutton").click()
+    # navigate to Kontoumsätze
+    driver.get("https://www.dkb.de/banking/finanzstatus/kontoumsaetze")
 
-# click csv export button
-driver.get("https://www.dkb.de/banking/finanzstatus/kontoumsaetze?$event=csvExport")
+    # set date range
+    from_date = driver.find_element_by_id("id-1615473160_transactionDate")
+    from_date.clear()
+    from_date.send_keys(FROM_DATE)
+    to_date = driver.find_element_by_id("id-1615473160_toTransactionDate")
+    to_date.clear()
+    to_date.send_keys(TO_DATE)
 
-print("Done")
+    # Loop trough all accounts and export the csv
+    select = driver.find_element_by_id("id-1615473160_slAllAccounts")
+    all_options = select.find_element_by_tag_name("option")
+    for option in all_options:
+        print(option.get_attribute("value"))
+        option.click()
+
+        # click on search button
+        driver.find_element_by_id("searchbutton").click()
+
+        # click csv export button
+        driver.get("https://www.dkb.de/banking/finanzstatus/kontoumsaetze?$event=csvExport")
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
+    
